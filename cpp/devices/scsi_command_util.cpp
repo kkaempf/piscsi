44162b42857ad6ebf4964c484775bfbc4b038c02
Klaus Kämpf <kkaempf@gmail.com>
2024-04-01 15:47:55 +0200

    Fix length calculation in scsi_command_util::ModeSelect
    OpenVMS Alpha sends a strange ModeSelect payload, apparently one byte
    too large. This was 'fixed' by a (wrong) length calculation in #1405, breaking #1427.
    
    This PR
    - fixes the wrong length calculation
    - improves the loop test in scsi_command_util::ModeSelect to prevent a
      buffer overflow. (Remaining length was checked for > 0, but buffer
      access is at offset and offset + 1, effectively requiring 2 bytes.)
    - the loop test fix makes #1402 pass
    - adds a testcase for #1402
    - adds a testcase for #1427
    
    Fixes issue #1427
    
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- cpp/devices/scsi_command_util.cpp
+++ cpp/devices/scsi_command_util.cpp
@@ -52,7 +52,8 @@ string scsi_command_util::ModeSelect(scsi_command cmd, cdb_t cdb, span<const uin
 	bool has_valid_page_code = (length == 0);
 
 	// Parse the pages
-	while (length > 0) {
+        // expect (remaining) length > 1 because we access buf[offset+1] below
+	while (length > 1) {
 		// Format device page
 		if (const int page = buf[offset]; page == 0x03) {
 			if (length < 14) {
